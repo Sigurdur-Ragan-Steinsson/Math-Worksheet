d864a4b8869e525d7cc7c9eb576e228d41914b25
Sar30 @ <108635097+Sigurdur-Ragan-Steinsson@users.noreply.github.com>
2022-07-04 21:32:56 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+ // Could also do in one line cout << "(10 * 7 + 40) + 38 * 10 = " << (10 * 7 + 40) + 38 * 10 << endl;
+int main() {
+  int q1 = (10 * 7 + 40) + 38 * 10;
+  cout << "(10 * 7 + 40) + 38 * 10 = ";
+  cout << q1 << endl;
+  int q2 = 31 - 1 * 5 - 8 + 33;
+  cout << "31 - 1 * 5 - 8 + 33 = ";
+  cout << q2 << endl;
+  int q3 = 2 * 2 + 36 + 15 - 17;
+  cout << "2 * 2 + 36 + 15 - 17 = ";
+  cout << q3 << endl;
+  int q4 = (4 * 9 / 2 * 10) + 40;
+  cout << "(4 * 9 / 2 * 10) + 40 = ";
+  cout << q4 << endl;
+  int q5 = 32 + 1 + (12 * 5 / 3);
+  cout << "32 + 1 + (12 * 5 / 3) = ";
+  cout << q5 << endl;
+}
